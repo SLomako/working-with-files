7b2300e94a8e968c588b35a6f89a5e11091d2c77
Slomako <lomakosv@gmail.com>
2023-07-03 00:13:01 +0300

    ref 3.07

--- src/test/java/ru/lomakosv/JSONObject.java
+++ src/test/java/ru/lomakosv/JSONObject.java
@@ -1,7 +1,10 @@
 package ru.lomakosv;
 
 public class JSONObject {
+    public String title;
+    public String groupName;
     public Album album;
+
 
     public static class Album {
         public String titleAlbum;
